cc3483361ce1d2ac2349e6926e3e2d673a9da154
Usman <manidevolper96@gmail.com>
2023-01-18 06:13:47 +0500

    responsive

--- components/InfoCards.tsx
+++ components/InfoCards.tsx
@@ -13,8 +13,8 @@ const InfoCards = ({
   total,
 }: Info) => {
   return (
-    <div className="flex ">
-      <div className="relative h-32 w-40 flex-shrink-0  md:h-52 md:w-80 "> 
+    <div className="flex  py-7 px-2 border-b cursor-pointer hover:opacity-80 pr-4 hover:shadow-lg flex-col md:flex-row transition duration-200 ease-out">
+      <div className="relative h-52 w-80 flex-shrink-0  md:h-52 md:w-80 "> 
         <Image
           src={img}
           fill
@@ -22,7 +22,7 @@ const InfoCards = ({
           className="absolute rounded-lg object-cover "
         />
       </div>
-      <div className="flex flex-grow flex-col pl-5">
+      <div className="flex flex-grow flex-col md:pl-5">
         <div className="flex  justify-between">
           <p>{location}</p>
           <HeartIcon className="h-7 cursor-pointer bg-white" />
@@ -30,11 +30,15 @@ const InfoCards = ({
         <h4 className="text-xl">{title}</h4>
         <div className="w-10 border-b pt-2  " />
         <p className="flex-grow pt-2 text-sm text-gray-500">{description}</p>
-        <div>
+        <div className="flex justify-between items-end pt-5">
           <p className='flex items-center '>
             <StarIcon className="h-5 text-red-400 " />
             {star}
           </p>
+          <div>
+            <p className='text-lg font-semibold pb-2 lg:text-2xl'>{price}</p>
+            <p className='text-right font-extralight '>{total}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -42,3 +46,4 @@ const InfoCards = ({
 };
 
 export default InfoCards;
+ 
